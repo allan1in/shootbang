f4a09ec87184547059819b37f423ae048abd2496
allan1in <allanlinemail@gmail.com>
2026-07-16 10:52:49 +0800

    fix: route Sentry browser events through tunnel

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,7 @@ export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   authToken: process.env.SENTRY_AUTH_TOKEN,
+  tunnelRoute: "/monitoring",
   silent: !process.env.CI,
   sourcemaps: {
     deleteSourcemapsAfterUpload: true,
